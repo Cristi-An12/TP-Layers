8c1c9953dea7d4a13d2b08dce5d2ef3c0c890616
Cristian <crsitan.gabimilla.12@gmail.com>
2025-05-02 15:49:09 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.swing.SwingUtilities;
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new Main().start();
+                } catch (Exception e) {
+// log exception...
+                    System.out.println(e);
+                }
+            }
+        });
+    }
+    private void start() {
+        new RadioCompetition();
+    }
+}
