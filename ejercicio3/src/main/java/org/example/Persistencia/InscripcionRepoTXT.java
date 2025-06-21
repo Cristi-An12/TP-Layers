a232c7183f79fa6a361bbc58a345e9f3f9ea7be9
Cristian <crsitan.gabimilla.12@gmail.com>
2025-06-21 12:22:41 -0300

    Agregar ejercicio3 como carpeta común

---
+++ ejercicio3/src/main/java/org/example/Persistencia/InscripcionRepoTXT.java
@@ -0,0 +1,35 @@
+package org.example.Persistencia;
+
+import org.example.Model.Inscripcion;
+import org.example.Model.InscripcionRepository;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class InscripcionRepoTXT implements InscripcionRepository {
+    private String rutaArchivo;
+
+    public InscripcionRepoTXT(String rutaArchivo) {
+        this.rutaArchivo = rutaArchivo;
+    }
+
+    @Override
+    public void guardarInscripcion (Inscripcion inscripcion) {
+        try(PrintWriter writer = new PrintWriter(new FileWriter (rutaArchivo, true))) {
+            String linea = formatearInscripcion(inscripcion);
+            writer.println (linea);
+        }catch (IOException e){
+            throw new RuntimeException ("Error al guardar inscripcion: "+ e.getMessage ());
+        }
+    }
+    private  String formatearInscripcion (Inscripcion inscripcion) {
+        return String.format("%s,%s,%s,%s,%s,%s",
+                inscripcion.getConcurso().getId(),
+                inscripcion.getConcurso().getNombre(),
+                inscripcion.getPersona().getNombre(),
+                inscripcion.getPersona().getApellido(),
+                inscripcion.getPersona().getDni(),
+                inscripcion.getPersona().getEmail());
+    }
+}
